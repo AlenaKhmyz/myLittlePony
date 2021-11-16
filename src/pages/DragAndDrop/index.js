eebbfee31d1e7853464b7163129728e48abc6f3d
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-11-16 14:12:05 +0300

    create card list

--- src/pages/DragAndDrop/index.js
+++ src/pages/DragAndDrop/index.js
@@ -1,6 +1,17 @@
+import React, {useEffect} from 'react'
+
 
 function DragAndDropPage () {
+  const [cardList, setCardList] = useState([
+    {id: 1, order: 3, text:'КАРТОЧКА 3'},
+    {id: 2, order: 1, text:'КАРТОЧКА 1'},
+    {id: 3, order: 2, text:'КАРТОЧКА 2'},
+    {id: 4, order: 4, text:'КАРТОЧКА 4'}
+  ])
 
+  return (
+    
+  )
 }
 
 export default DragAndDropPage
