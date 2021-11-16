66569661eb439ad80134e628d0248c997baf90f3
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-11-16 18:10:51 +0300

    added function sort

--- src/pages/DragAndDrop/index.js
+++ src/pages/DragAndDrop/index.js
@@ -9,16 +9,61 @@ function DragAndDropPage () {
     {id: 3, order: 2, text:'КАРТОЧКА 2'},
     {id: 4, order: 4, text:'КАРТОЧКА 4'}
   ])
+//т.к. будем карточки менять местами, нужно запоминать карточку создадим новое состояние
+  const [currentCard, setCurrentCard] = useState(null)
+
+  function dragEndHandler() {
+    setCurrentCard(card)
+  }
+
+  function dragEndHandler() {
+    e.target.style.background = 'white'
+  }
+
+  function dragOvertHandler(e) {
+    e.preventDefault(e)
+    e.target.style.background = 'lightgray'
+  }
+
+  function dragHandler(e, card) {
+    e.preventDefault()
+//логика сортировка карточки(меняем их местами)
+    setCardList(cardList.map(c => {
+      if(c.id === card.id) {
+        return{...c, order: currentCard.order}
+      }
+
+      if(c.id === currentCard.id) {
+        return{...c, order: card.order}
+      }
+
+      return c
+    }))
+
+    e.target.style.background="white"
+  }
+
+//сортировка по порядку
+  const sortCards = (a, b) => {
+    if (a.order > b.order) {
+      return 1
+    } else {
+      return -1
+    }
+  }
 
   return (
     <div className="cardList">
-      {cardList.map( card=> 
+      {cardList.sort(sortCards).map( card=> 
         <div className="card"
-          onDragStart={}
-          onDragLeave={}
-          onDragEnd={}
-          onDragOver={}
-          onDrop={}
+//для реализации drag and drop нужно реализовать несколько слушателей событий
+//для каждого из этих слушателей необходимо создать функцию, которая будет вызываться.
+//далее создаем выше каждую из этих функций
+          onDragStart={(e) => dragStartHandler(e, card)} //срабатывает в тот момент, когда мы взяли карточку
+          onDragLeave={(e) => dragEndHandler(e)} //срабатывает, если мы вышли за пределы другой карточки
+          onDragEnd={(e) => dragEndHandler(e)} //если мы отпустили перемещение
+          onDragOver={(e) => dragOvertHandler(e)} //если мы находимся над каким-то другим объектом
+          onDrop={(e) => dragHandler(e, card)} //если мы отпустили карточку и рассчитываем на то, что что должно произойти связанное с эти действие
           draggable={true}>
           {card.text}
         </div>
