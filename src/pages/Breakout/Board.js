a090662ba24f7bda5bba4e011fa44c32b795facb
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-12-03 01:53:52 +0300

    added circle

--- src/pages/Breakout/Board.js
+++ src/pages/Breakout/Board.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 
+
+let x = 0
 export default function Board() {
+
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const render = () => {
+      const canvas = canvasRef.current
+      const ctx = canvas.getContext('2d')
+      ctx.beginPath();
+      ctx.arc(x, 50, 50, 0, 2 * Math.PI, false);
+      ctx.stroke();
+      x++
+      requestAnimationFrame(render)
+    }
+    
+    render()
+  }, [])
+
+
   return (
-    <canvas id="canvas" height="500px" width="800px"></canvas>
+    <canvas id="canvas" ref={canvasRef} height="500px" width="800px"></canvas>
   )
 }
